5a676057779a9963e2e9ce941f8a856fedf95393
pengdst <pengkuh.dst@gmail.com>
2021-12-10 15:07:10 +0700

    handle if session is not logged in

--- app/src/main/java/io/github/pengdst/salescashier/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/main/MainActivity.kt
@@ -1,18 +1,32 @@
 package io.github.pengdst.salescashier.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.pengdst.libs.ui.activity.viewbinding.ActivityViewBindingDelegate.Companion.viewBindings
+import io.github.pengdst.salescashier.data.local.prefs.Session
 import io.github.pengdst.salescashier.databinding.ActivityMainBinding
+import io.github.pengdst.salescashier.ui.login.LoginActivity
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by viewBindings()
 
+    @Inject lateinit var session: Session
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!session.isLogin()) startActivity(Intent(applicationContext, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }).also {
+            finish()
+            return
+        }
+
         setContentView(binding.root)
     }
 }
