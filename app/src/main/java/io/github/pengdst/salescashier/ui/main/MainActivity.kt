e7b39cee70779fbb54ab3889b4936cf89fcf34f6
pengdst <pengkuh.dst@gmail.com>
2021-12-10 15:42:34 +0700

    setup bottom nav and toolbar with navcontroller

--- app/src/main/java/io/github/pengdst/salescashier/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/main/MainActivity.kt
@@ -3,8 +3,13 @@ package io.github.pengdst.salescashier.ui.main
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.pengdst.libs.ui.activity.viewbinding.ActivityViewBindingDelegate.Companion.viewBindings
+import io.github.pengdst.salescashier.R
 import io.github.pengdst.salescashier.data.local.prefs.Session
 import io.github.pengdst.salescashier.databinding.ActivityMainBinding
 import io.github.pengdst.salescashier.ui.login.LoginActivity
@@ -15,7 +20,14 @@ class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by viewBindings()
 
-    @Inject lateinit var session: Session
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment
+        navHostFragment.navController
+    }
+
+    @Inject
+    lateinit var session: Session
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +40,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(binding.root)
+
+        with(binding) {
+            toolbar.setupWithNavController(navController, AppBarConfiguration(
+                setOf(
+                    R.id.productFragment,
+                    R.id.transactionFragment,
+                    R.id.reportFragment,
+                    R.id.aboutFragment,
+                )
+            ))
+            bottomNav.setupWithNavController(navController)
+        }
     }
 }
