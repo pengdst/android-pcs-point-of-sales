fec947e15227d591e0d5b230cf25c7d2e799fe60
pengdst <pengkuh.dst@gmail.com>
2021-12-10 17:26:27 +0700

    add route get products

---
+++ app/src/main/java/io/github/pengdst/salescashier/data/remote/models/Product.kt
@@ -0,0 +1,17 @@
+package io.github.pengdst.salescashier.data.remote.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Product(
+    @SerializedName("admin_id")
+    val adminId: Int? = null,
+    @SerializedName("harga")
+    val harga: Double? = null,
+    @SerializedName("id")
+    val id: Int? = null,
+    @SerializedName("nama")
+    val nama: String? = null,
+    @SerializedName("stock")
+    val stock: Int? = null
+)
--- app/src/main/java/io/github/pengdst/salescashier/data/remote/routes/SalesRoute.kt
+++ app/src/main/java/io/github/pengdst/salescashier/data/remote/routes/SalesRoute.kt
@@ -1,10 +1,12 @@
 package io.github.pengdst.salescashier.data.remote.routes
 
 import io.github.pengdst.salescashier.data.remote.models.AuthData
+import io.github.pengdst.salescashier.data.remote.models.Product
 import io.github.pengdst.salescashier.data.remote.responses.SalesResponse
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface SalesRoute {
@@ -15,5 +17,8 @@ interface SalesRoute {
         @Field("email") email: String?,
         @Field("password") password: String?
     ): Response<SalesResponse<AuthData>>
+
+    @GET("product")
+    suspend fun getProducts(): Response<SalesResponse<List<Product>>>
 
 }
