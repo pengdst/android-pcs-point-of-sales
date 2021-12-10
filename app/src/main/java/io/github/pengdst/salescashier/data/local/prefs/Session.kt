21e7bc27480493ddb03f0721458d9522399825b0
pengdst <pengkuh.dst@gmail.com>
2021-12-10 15:05:08 +0700

    create Session.kt

---
+++ app/src/main/java/io/github/pengdst/salescashier/data/local/prefs/Session.kt
@@ -0,0 +1,23 @@
+package io.github.pengdst.salescashier.data.local.prefs
+
+import io.github.pengdst.salescashier.data.constants.AuthConst
+import io.github.pengdst.salescashier.data.remote.models.Admin
+
+class Session(private val sessionHelper: SessionHelper) {
+
+    fun isLogin() = !sessionHelper.getString(AuthConst.KEY_AUTH_TOKEN).isNullOrEmpty()
+
+    fun saveUser(admin: Admin, authKey: String?) = sessionHelper.apply {
+        save(AuthConst.KEY_ID, admin.id)
+        save(AuthConst.KEY_EMAIL, admin.email)
+        save(AuthConst.KEY_NAME, admin.nama)
+        save(AuthConst.KEY_AUTH_TOKEN, authKey)
+    }
+
+    fun getAuthUser() = Admin(
+        id = sessionHelper.getInt(AuthConst.KEY_ID),
+        email = sessionHelper.getString(AuthConst.KEY_EMAIL),
+        nama = sessionHelper.getString(AuthConst.KEY_ID)
+    )
+
+}
