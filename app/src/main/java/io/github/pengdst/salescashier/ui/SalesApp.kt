899ea5658d9fa8c849b8c1cf9d670b7dfce3420d
pengdst <pengkuh.dst@gmail.com>
2021-12-10 15:02:27 +0700

    setup Timber

--- app/src/main/java/io/github/pengdst/salescashier/ui/SalesApp.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/SalesApp.kt
@@ -2,6 +2,16 @@ package io.github.pengdst.salescashier.ui
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.github.pengdst.salescashier.BuildConfig
+import timber.log.Timber
 
 @HiltAndroidApp
-class SalesApp : Application()
+class SalesApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+}
