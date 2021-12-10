96bbb7a82f4920d398a875e3aba752f2174f8856
pengdst <pengkuh.dst@gmail.com>
2021-12-10 15:05:33 +0700

    provide SessionHelper and Session

--- app/src/main/java/io/github/pengdst/salescashier/di/AppComponent.kt
+++ app/src/main/java/io/github/pengdst/salescashier/di/AppComponent.kt
@@ -1,9 +1,13 @@
 package io.github.pengdst.salescashier.di
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.github.pengdst.salescashier.data.local.prefs.Session
+import io.github.pengdst.salescashier.data.local.prefs.SessionHelper
 import io.github.pengdst.salescashier.data.remote.routes.SalesRoute
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -24,4 +28,13 @@ object AppComponent {
     @Provides
     @Singleton
     fun provideSalesRoute(retrofit: Retrofit): SalesRoute = retrofit.create(SalesRoute::class.java)
+
+    @Provides
+    @Singleton
+    fun provideSessionHelper(@ApplicationContext context: Context): SessionHelper = SessionHelper
+        .newInstance(context)
+
+    @Provides
+    @Singleton
+    fun provideSession(sessionHelper: SessionHelper): Session = Session(sessionHelper)
 }
