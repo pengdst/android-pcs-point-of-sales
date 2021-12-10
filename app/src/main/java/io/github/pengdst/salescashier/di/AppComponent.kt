53d276f14c16dcddb6b970c498f5c0520b463c1d
pengdst <pengkuh.dst@gmail.com>
2021-12-10 17:22:43 +0700

    provide number format and glide

--- app/src/main/java/io/github/pengdst/salescashier/di/AppComponent.kt
+++ app/src/main/java/io/github/pengdst/salescashier/di/AppComponent.kt
@@ -1,11 +1,15 @@
 package io.github.pengdst.salescashier.di
 
 import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.github.pengdst.salescashier.R
 import io.github.pengdst.salescashier.data.local.prefs.Session
 import io.github.pengdst.salescashier.data.local.prefs.SessionHelper
 import io.github.pengdst.salescashier.data.remote.routes.SalesRoute
@@ -13,6 +17,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 import okhttp3.OkHttpClient
+import java.text.NumberFormat
+import java.util.*
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -48,4 +54,19 @@ object AppComponent {
     @Provides
     @Singleton
     fun provideSession(sessionHelper: SessionHelper): Session = Session(sessionHelper)
+
+    @Provides
+    @Singleton
+    fun provideNumberFormat(): NumberFormat {
+        val localeID = Locale("in", "ID")
+        return NumberFormat.getCurrencyInstance(localeID)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGlideRequestManager(@ApplicationContext context: Context) = Glide.with(context)
+        .applyDefaultRequestOptions(
+            RequestOptions().placeholder(R.color.white).error(R.drawable.ic_about)
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+        )
 }
