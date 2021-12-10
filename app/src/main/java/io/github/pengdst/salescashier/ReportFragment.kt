31105e75f5d4d9d41bec1ba510ee57ebc7cbf7a3
pengdst <pengkuh.dst@gmail.com>
2021-12-10 07:17:11 +0700

    create fragments

---
+++ app/src/main/java/io/github/pengdst/salescashier/ReportFragment.kt
@@ -0,0 +1,18 @@
+package io.github.pengdst.salescashier
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ReportFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_report, container, false)
+    }
+}
