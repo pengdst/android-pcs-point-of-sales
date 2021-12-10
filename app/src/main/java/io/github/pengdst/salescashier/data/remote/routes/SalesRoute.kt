1282e364a96cf3a05b282ff0a55ae6e8a9c7ae58
pengdst <pengkuh.dst@gmail.com>
2021-12-10 15:06:16 +0700

    add FormUrlEncoded

--- app/src/main/java/io/github/pengdst/salescashier/data/remote/routes/SalesRoute.kt
+++ app/src/main/java/io/github/pengdst/salescashier/data/remote/routes/SalesRoute.kt
@@ -4,10 +4,12 @@ import io.github.pengdst.salescashier.data.remote.models.AuthData
 import io.github.pengdst.salescashier.data.remote.responses.SalesResponse
 import retrofit2.Response
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface SalesRoute {
 
+    @FormUrlEncoded
     @POST("auth/login")
     suspend fun login(
         @Field("email") email: String?,
