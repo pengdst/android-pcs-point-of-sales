14349cbc31bc550eedfe49b45262f933949e565e
pengdst <pengkuh.dst@gmail.com>
2021-12-10 15:05:56 +0700

    create AuthConst.kt

---
+++ app/src/main/java/io/github/pengdst/salescashier/data/constants/AuthConst.kt
@@ -0,0 +1,10 @@
+package io.github.pengdst.salescashier.data.constants
+
+object AuthConst {
+
+    const val KEY_AUTH_TOKEN = "auth_token"
+    const val KEY_ID = "auth_id"
+    const val KEY_NAME = "auth_name"
+    const val KEY_EMAIL = "auth_email"
+
+}
