96c2a3ae6bf051f4b736e65dee090d783989898b
pengdst <pengkuh.dst@gmail.com>
2021-12-15 19:01:52 +0700

    feature report

--- app/src/main/java/io/github/pengdst/salescashier/ui/report/ReportFragment.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/report/ReportFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.pengdst.libs.ui.fragment.viewbinding.FragmentViewBindingDelegate.Companion.viewBindings
+import io.github.pengdst.salescashier.data.remote.models.TransactionItem
+import io.github.pengdst.salescashier.data.remote.responses.ErrorResponse
+import io.github.pengdst.salescashier.data.remote.routes.SalesRoute
 import io.github.pengdst.salescashier.databinding.FragmentReportBinding
+import io.github.pengdst.salescashier.utils.longToast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 import java.text.NumberFormat
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,6 +27,10 @@ class ReportFragment : Fragment() {
 
     @Inject
     lateinit var numberFormat: NumberFormat
+    @Inject
+    lateinit var reportAdapter: ReportAdapter
+    @Inject
+    lateinit var salesRoute: SalesRoute
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +43,72 @@ class ReportFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tvTotalRevenue.text = numberFormat.format(0)
+        with(binding) {
+            tvTotalRevenue.text = numberFormat.format(0)
+            rvReports.adapter = reportAdapter
+        }
+
+        getTransaction()
+    }
+
+    private fun getTransaction() {
+        lifecycleScope.launchWhenCreated {
+            try {
+                withContext(Dispatchers.IO) {
+                    val response = salesRoute.getTransactions()
+                    if (response.isSuccessful) {
+                        val responseBody = response.body()
+
+                        val transactionItems = getTransactionItems()
+
+                        val transactions = responseBody?.data?.map { transaction ->
+                            transaction.apply {
+                                items = transactionItems.filter { it.transaksiId == transaction.id }
+                            }
+                        } ?: emptyList()
+
+
+                        withContext(Dispatchers.Main) {
+                            binding.tvTotalRevenue.text = numberFormat.format(
+                                transactions.filter {
+                                    val calendar = Calendar.getInstance()
+                                    val currentMonth = calendar[Calendar.MONTH]
+                                    calendar.timeInMillis = it.date.time
+                                    Timber.e("getTransaction() ${currentMonth} == ${calendar[Calendar.MONTH]} called")
+                                    currentMonth == calendar[Calendar.MONTH]
+                                }.sumOf {
+                                    it.items.sumOf {
+                                        it.priceTotal
+                                    }
+                                }
+                            )
+                            reportAdapter.submitList(transactions)
+                        }
+                    } else {
+                        val errorBody = ErrorResponse.fromErrorBody(response.errorBody())
+                        withContext(Dispatchers.Main) {
+                            longToast(errorBody.message ?: "Show Products Failed")
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
+    }
+
+    private suspend fun getTransactionItems(): List<TransactionItem> {
+        val response = salesRoute.getTransactionItems()
+        if (response.isSuccessful) {
+            val responseBody = response.body()
+
+            return responseBody?.data ?: emptyList()
+        } else {
+            val errorBody = ErrorResponse.fromErrorBody(response.errorBody())
+            withContext(Dispatchers.Main) {
+                longToast(errorBody.message ?: "Show Products Failed")
+            }
+        }
+        return emptyList()
     }
 }
