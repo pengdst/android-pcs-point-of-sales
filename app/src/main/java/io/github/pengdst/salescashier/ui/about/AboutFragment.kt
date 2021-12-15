52c5aee603e329ccb0283a9b279ec612e932bea7
pengdst <pengkuh.dst@gmail.com>
2021-12-15 19:12:24 +0700

    create AboutFragment.kt and fragment_about.xml

---
+++ app/src/main/java/io/github/pengdst/salescashier/ui/about/AboutFragment.kt
@@ -0,0 +1,46 @@
+package io.github.pengdst.salescashier.ui.about
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.pengdst.libs.ui.fragment.viewbinding.FragmentViewBindingDelegate.Companion.viewBindings
+import io.github.pengdst.salescashier.data.local.prefs.Session
+import io.github.pengdst.salescashier.databinding.FragmentAboutBinding
+import io.github.pengdst.salescashier.ui.login.LoginActivity
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class AboutFragment : Fragment() {
+
+    private val binding: FragmentAboutBinding by viewBindings()
+
+    @Inject
+    lateinit var session: Session
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            btnLogout.setOnClickListener {
+                session.logout()
+                startActivity(Intent(requireContext(), LoginActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                }).also {
+                    requireActivity().finish()
+                }
+            }
+        }
+    }
+}
