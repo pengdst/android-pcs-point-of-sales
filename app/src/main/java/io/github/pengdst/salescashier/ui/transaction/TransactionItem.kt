0e224e77267c0a8791974a3525284a0532d2d5e4
pengdst <pengkuh.dst@gmail.com>
2021-12-15 17:12:51 +0700

    fix goto PayTransactionFragment.kt

--- app/src/main/java/io/github/pengdst/salescashier/ui/transaction/TransactionItem.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/transaction/TransactionItem.kt
@@ -1,8 +1,11 @@
 package io.github.pengdst.salescashier.ui.transaction
 
+import android.os.Parcelable
 import io.github.pengdst.salescashier.data.remote.models.Product
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class TransactionItem (
     val product: Product,
     var amount: Int = 0
-)
+) : Parcelable
--- app/src/main/java/io/github/pengdst/salescashier/ui/transaction/pay/PayTransactionFragment.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/transaction/pay/PayTransactionFragment.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.pengdst.libs.ui.fragment.viewbinding.FragmentViewBindingDelegate.Companion.viewBindings
 import io.github.pengdst.salescashier.databinding.FragmentPayTransactionBinding
+import java.text.NumberFormat
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class PayTransactionFragment : Fragment() {
 
     private val binding: FragmentPayTransactionBinding by viewBindings()
+
+    private val args: PayTransactionFragmentArgs by navArgs()
+
+    @Inject lateinit var numberFormat: NumberFormat
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +27,16 @@ class PayTransactionFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            val sumOfPrice = args.productTransactions.toList().sumOf {
+                it.amount * (it.product.harga ?: 0.0)
+            }
+            tvTotalPay.text = numberFormat.format(sumOfPrice)
+        }
     }
 }
