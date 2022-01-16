7383bdbf3117fec48c0c29bc081d29d20738137b
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-01-17 05:51:57 +0700

    add update and create product

--- app/src/main/java/io/github/pengdst/salescashier/ui/product/ProductViewModel.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/product/ProductViewModel.kt
@@ -1,10 +1,9 @@
 package io.github.pengdst.salescashier.ui.product
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import io.github.pengdst.salescashier.data.remote.models.Product
+import io.github.pengdst.salescashier.data.remote.requests.CreateProductRequest
+import io.github.pengdst.salescashier.data.remote.requests.UpdateProductRequest
 import io.github.pengdst.salescashier.data.repositories.SalesRepository
 import io.github.pengdst.salescashier.data.vo.ResultWrapper
 import kotlinx.coroutines.launch
@@ -41,9 +40,31 @@ class ProductViewModel @Inject constructor(
         }
     }
 
+    fun createProduct(request: CreateProductRequest) {
+        _productsViewModel.postValue(State.Loading)
+        viewModelScope.launch {
+            when(val res = repository.createProduct(request)) {
+                is ResultWrapper.Error -> _productsViewModel.value = State.SuccessCreate(res.data ?: return@launch, res.message)
+                is ResultWrapper.Success -> _productsViewModel.value = State.Failed(res.message)
+            }
+        }
+    }
+
+    fun updateProduct(productId: Int?, request: UpdateProductRequest) {
+        _productsViewModel.postValue(State.Loading)
+        viewModelScope.launch {
+            when(val res = repository.updateProduct(productId, request)) {
+                is ResultWrapper.Error -> _productsViewModel.value = State.SuccessUpdate(res.data ?: return@launch, res.message)
+                is ResultWrapper.Success -> _productsViewModel.value = State.Failed(res.message)
+            }
+        }
+    }
+
     sealed class State {
         data class Success(val data: List<Product>, val message: String): State()
         data class SuccessDelete(val data: List<Product>, val message: String): State()
+        data class SuccessUpdate(val data: Product, val message: String): State()
+        data class SuccessCreate(val data: Product, val message: String): State()
         data class Failed(val message: String): State()
         object Loading: State()
     }
