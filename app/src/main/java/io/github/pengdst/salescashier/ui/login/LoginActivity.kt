73903156eb87a0b8c0d6c8424567d8994b96f6fa
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-01-17 05:20:24 +0700

    use LoginViewModel.kt in LoginActivity.kt

--- app/src/main/java/io/github/pengdst/salescashier/ui/login/LoginActivity.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/login/LoginActivity.kt
@@ -2,33 +2,49 @@ package io.github.pengdst.salescashier.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.pengdst.libs.ui.activity.viewbinding.ActivityViewBindingDelegate.Companion.viewBindings
-import io.github.pengdst.salescashier.data.local.prefs.Session
-import io.github.pengdst.salescashier.data.remote.responses.ErrorResponse
-import io.github.pengdst.salescashier.data.remote.routes.SalesRoute
 import io.github.pengdst.salescashier.databinding.ActivityLoginBinding
 import io.github.pengdst.salescashier.ui.main.MainActivity
 import io.github.pengdst.salescashier.utils.longToast
 import io.github.pengdst.salescashier.utils.shortToast
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import timber.log.Timber
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
     private val binding: ActivityLoginBinding by viewBindings()
 
-    @Inject lateinit var salesRoute: SalesRoute
-    @Inject lateinit var session: Session
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        loginViewModel.loginViewModel.observe(this) {
+            binding.btnLogin.isEnabled = true
+            when(it) {
+                is LoginViewModel.State.Failed -> {
+                    longToast(it.message)
+                }
+                is LoginViewModel.State.Loading -> {
+                    binding.btnLogin.isEnabled = false
+                }
+                is LoginViewModel.State.Success -> {
+                    startActivity(Intent(applicationContext, MainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    }).also {
+                        finish()
+                    }
+                    shortToast(it.message)
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
 
         with(binding) {
 
@@ -36,43 +52,7 @@ class LoginActivity : AppCompatActivity() {
                 val email = tilEmail.editText?.text.toString().trim()
                 val password = tilPassword.editText?.text.toString().trim()
 
-                login(email, password)
-            }
-        }
-    }
-
-    fun login(email: String, password: String) {
-        binding.btnLogin.isEnabled = false
-        lifecycleScope.launchWhenCreated {
-            try {
-                withContext(Dispatchers.IO) {
-                    val response = salesRoute.login(email, password)
-                    if (response.isSuccessful) {
-                        val responseBody = response.body()
-
-                        val loginData = responseBody?.data ?: return@withContext
-
-                        session.saveUser(loginData.admin ?: return@withContext, loginData.token)
-
-                        withContext(Dispatchers.Main) {
-                            startActivity(Intent(applicationContext, MainActivity::class.java).apply {
-                                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                            }).also {
-                                finish()
-                            }
-                            shortToast(responseBody.message)
-                        }
-                    } else {
-                        val errorBody = ErrorResponse.fromErrorBody(response.errorBody())
-                        withContext(Dispatchers.Main) {
-                            longToast(errorBody.message ?: "Login Failed")
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                Timber.e(e)
-            } finally {
-                binding.btnLogin.isEnabled = true
+                loginViewModel.login(email, password)
             }
         }
     }
