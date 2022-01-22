7547414dd2215a8d6185d1283af5d575ec71352b
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-01-22 14:49:59 +0700

    use TransactionViewModel.kt in PayTransactionFragment.kt

--- app/src/main/java/io/github/pengdst/salescashier/ui/transaction/pay/PayTransactionFragment.kt
+++ app/src/main/java/io/github/pengdst/salescashier/ui/transaction/pay/PayTransactionFragment.kt
@@ -6,17 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.pengdst.libs.ui.fragment.viewbinding.FragmentViewBindingDelegate.Companion.viewBindings
-import io.github.pengdst.salescashier.data.local.prefs.Session
-import io.github.pengdst.salescashier.data.remote.models.Transaction
-import io.github.pengdst.salescashier.data.remote.requests.CreateItemTransactionRequest
 import io.github.pengdst.salescashier.data.remote.requests.CreateTransactionRequest
-import io.github.pengdst.salescashier.data.remote.responses.ErrorResponse
-import io.github.pengdst.salescashier.data.remote.routes.SalesRoute
 import io.github.pengdst.salescashier.databinding.FragmentPayTransactionBinding
+import io.github.pengdst.salescashier.ui.transaction.TransactionViewModel
 import io.github.pengdst.salescashier.utils.longToast
 import io.github.pengdst.salescashier.utils.shortToast
 import kotlinx.coroutines.Dispatchers
@@ -33,20 +30,28 @@ class PayTransactionFragment : Fragment() {
 
     private val args: PayTransactionFragmentArgs by navArgs()
 
+    private val transactionViewModel: TransactionViewModel by viewModels()
+
     @Inject
     lateinit var numberFormat: NumberFormat
-
-    @Inject
-    lateinit var session: Session
-
-    @Inject
-    lateinit var salesRoute: SalesRoute
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
+        transactionViewModel.productsViewModel.observe(viewLifecycleOwner) {
+            when(it) {
+                is TransactionViewModel.State.Loading -> {
+
+                    requireActivity().onBackPressed()
+                }
+                is TransactionViewModel.State.SuccessCreateItem -> shortToast(it.message)
+                is TransactionViewModel.State.SuccessPayTransaction -> shortToast(it.message)
+                is TransactionViewModel.State.Failed -> longToast(it.message)
+            }
+        }
+
         return binding.root
     }
 
@@ -78,64 +83,11 @@ class PayTransactionFragment : Fragment() {
         lifecycleScope.launchWhenResumed {
             try {
                 withContext(Dispatchers.IO) {
-                    val response = salesRoute.createTransaction(
-                        CreateTransactionRequest(
-                            adminId = session.getAuthUser().id,
-                            total = args.productTransactions.size
-                        )
+                    val createTransactionRequest = CreateTransactionRequest(
+                        total = args.productTransactions.size
                     )
-                    if (response.isSuccessful) {
-                        val responseBody = response.body()
-
-                        val products = responseBody?.data
-
-                        withContext(Dispatchers.Main) {
-                            shortToast(responseBody?.message ?: "Success Create Transaction")
-                            products?.let { createTransactionItem(it) }
-                        }
-                    } else {
-                        val errorBody = ErrorResponse.fromErrorBody(response.errorBody())
-                        withContext(Dispatchers.Main) {
-                            longToast(errorBody.message ?: "Create Transaction Failed")
-                        }
-                    }
+                    transactionViewModel.payTransaction(createTransactionRequest, args.productTransactions.toList())
                 }
-            } catch (e: Exception) {
-                Timber.e(e)
-            } finally {
-                binding.btnPay.isEnabled = true
-            }
-        }
-    }
-
-    private fun createTransactionItem(transaction: Transaction) {
-        binding.btnPay.isEnabled = false
-        lifecycleScope.launchWhenResumed {
-            try {
-                args.productTransactions.forEach {
-                    withContext(Dispatchers.IO) {
-                        val response = salesRoute.createTransactionItem(
-                            CreateItemTransactionRequest(
-                                productId = it.product.id,
-                                harga = it.product.harga,
-                                qty = it.amount,
-                                transaksiId = transaction.id
-                            )
-                        )
-                        if (response.isSuccessful) {
-                            val responseBody = response.body()
-                            withContext(Dispatchers.Main) {
-                                shortToast(responseBody?.message ?: "Success Create Transaction Item")
-                            }
-                        } else {
-                            val errorBody = ErrorResponse.fromErrorBody(response.errorBody())
-                            withContext(Dispatchers.Main) {
-                                longToast(errorBody.message ?: "Create Transaction Item Failed")
-                            }
-                        }
-                    }
-                }
-                requireActivity().onBackPressed()
             } catch (e: Exception) {
                 Timber.e(e)
             } finally {
